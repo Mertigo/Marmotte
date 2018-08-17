8d453e5e2a163b62243bae6e7fbdec6663c99090
Mertigo <42481819+Mertigo@users.noreply.github.com>
2018-08-18 01:05:41 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+bot.on('ready', function () {
+    bot.user.setGame('Warframe').catch(console.error)
+})
+
+
+
+bot.on('message', function (message) {
+   if (message.content === '/marmotte') {
+     message.channel.send('Master marmotte mertigo')  
+   } 
+}) 
+
+bot.login('NDc4OTg5MDk1OTYwNDQ0OTI4.DlTwSA.xjhq3ZHvf96gYqj86wfy1rxRanQ')
